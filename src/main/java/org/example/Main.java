c1267f42c6d5eaeb2518786716d717d19fbfba63
Acunya <543313533@qq.com>
2024-04-09 23:54:21 +0800

    0409

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("pushTest");
+        System.out.println("pushTest2");
     }
 }
